bef6f2408f166b76ad13f29b6c1e783d0b8196c7
Matt <mjmulvahill@gmail.com>
2018-04-04 16:11:02 -0600

    Initial commit -- added readme, .gitignore, and week 1's data & initial script.

---
+++ R/week1_ustuitioncosts.R
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# week1_ustuitioncosts.R
+#   First week's exercise for R4DS's #TidyTuesday.
+#
+# Author: Matt Mulvahill
+# week1 == 20180403
+#-------------------------------------------------------------------------------
+
+
+library(tidyverse)
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# mytidytuesdays
+
+A repository for story my #TidyTuesday exercises.  #TidyTuesday is a weekly
+social data project put on by the R4DS (R for Data Science) community. The
+project repo is located
+[here](https://github.com/rfordatascience/tidytuesday/blob/master/README.md)
+
+
